2d6af5a58e958b5cfbd0bcf0d64edd550e34f465
popoyorc <badordionicio@gmail.com>
2020-07-16 17:22:29 +0800

    Update lustre.sh

--- lustre.sh
+++ lustre.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-hostname
-#mkdir -p /etc/zabbix/scripts/agentd/lustre/
-#cat << EOF
-##!/bin/bash
-#OUT=`lctl ping $(ifconfig bond0 | grep inet | awk '{print $2}' | cut -f2 -d:) 2>/dev/null | grep -e "12345-" | wc -l`
-#if [[ ${OUT} == '2' ]]; then
-#        echo "0"
-#else
-#        echo "1"
-#fi
-#EOF > /etc/zabbix/scripts/agentd/lustre/lustre.sh
-#chmod +x /etc/zabbix/scripts/agentd/lustre/lustre.sh
-#cat "UserParameter=lustre.ping,/etc/zabbix/scripts/agentd/lustre/lustre.sh" > /etc/zabbix/zabbix_agent.d/lustre.conf
+echo "=================="$(hostname)"==================="
+mkdir -p /etc/zabbix/scripts/agentd/lustre/
+cat << EOF
+#!/bin/bash
+OUT=`lctl ping $(ifconfig bond0 | grep inet | awk '{print $2}' | cut -f2 -d:) 2>/dev/null | grep -e "12345-" | wc -l`
+if [[ ${OUT} == '2' ]]; then
+        echo "0"
+else
+        echo "1"
+fi
+EOF > /etc/zabbix/scripts/agentd/lustre/lustre.sh
+chmod +x /etc/zabbix/scripts/agentd/lustre/lustre.sh
+cat "UserParameter=lustre.ping,/etc/zabbix/scripts/agentd/lustre/lustre.sh" > /etc/zabbix/zabbix_agent.d/lustre.conf
 systemctl restart zabbix-agent
