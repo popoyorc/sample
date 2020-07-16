43079380e7f57dccd1737abe2c91a47d84ad832e
popoyorc <badordionicio@gmail.com>
2020-07-16 17:43:24 +0800

    Update and rename lustre.sh to zabb.sh

--- zabb.sh
+++ zabb.sh
@@ -2,9 +2,9 @@
 
 echo "=================="$(hostname)"==================="
 mkdir -p /etc/zabbix/scripts/agentd/lustre/
-cat <<EOF > /etc/zabbix/scripts/agentd/lustre/lustre.sh
+cat <<\EOF > /etc/zabbix/scripts/agentd/lustre/lustre.sh
 #!/bin/bash
-OUT=`lctl ping $(ifconfig bond0 | grep inet | awk '{print $2}' | cut -f2 -d:) 2>/dev/null | grep -e "12345-" | wc -l`
+OUT=`lctl ping $(ifconfig bond0 | grep inet | awk '{print $2}' | cut -f2 -d:) 2>/dev/null | grep -e '12345-' | wc -l`
 if [[ ${OUT} == '2' ]]; then
         echo "0"
 else
