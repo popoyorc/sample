4be75c4632e13dc017f18ac6edcd5abc83cef956
popoyorc <badordionicio@gmail.com>
2020-07-16 17:47:29 +0800

    Update zabb.sh

--- zabb.sh
+++ zabb.sh
@@ -12,6 +12,5 @@ else
 fi
 EOF
 chmod +x /etc/zabbix/scripts/agentd/lustre/lustre.sh
-touch /etc/zabbix/zabbix_agent.d/lustre.conf
-cat "UserParameter=lustre.ping,/etc/zabbix/scripts/agentd/lustre/lustre.sh" > /etc/zabbix/zabbix_agent.d/lustre.conf
+cat "UserParameter=lustre.ping,/etc/zabbix/scripts/agentd/lustre/lustre.sh" > /etc/zabbix/zabbix_agentd.d/lustre.conf
 systemctl restart zabbix-agent
